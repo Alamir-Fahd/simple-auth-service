55b9a48b9678bcf36a171ee4986461c9964b139f
Alamir-Fahd <amirfahd244@gmail.com>
2026-02-16 07:43:45 +0200

    Update users.js

--- src/users.js
+++ src/users.js
@@ -1,7 +1,7 @@
-const users = [
-  { id: 1, username: "admin", password: "admin123", role: "admin" },
-  { id: 2, username: "user", password: "user123", role: "user" }
-];
+const fs = require('fs');
+const path = require('path');
+const dataPath = path.join(__dirname, '../data/users.json');
+const users = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
 
 function findUser(username) {
   return users.find(u => u.username === username);
